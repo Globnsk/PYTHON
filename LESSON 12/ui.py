bdcfa38a9812e37cda9b818d4fb0d4320d7cffc6
Alexander Glotov <glob.nsk@mail.ru>
2024-06-24 18:08:01 +0300

    Add LESSON 12

--- LESSON 12/ui.py
+++ LESSON 12/ui.py
@@ -1,11 +1,16 @@
+from logger import input_data, print_data
 
-
-def intrface():
-    print ("Добрый день! Вы попали на спец бот-справочник!\n 1 - Запись данных \n  2 - Вывод данных")
-    command = int (input('Введите число'))
+def interface():
+    print ("Добрый день! Вы попали на спец бот-справочник!\n 1 - Запись данных \n 2 - Вывод данных")
+    command = int (input('Введите число '))
 
     while command != 1 and command != 2:
         print("Неправильный ввод")
         command = int (input('Введите число '))
 
-interface()
+#interface()
+
+    if command == 1:
+        input_data()
+    elif command == 2:
+        print_data ()
